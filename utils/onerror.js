89be78e26891c0866b0cbac2089197f0a6fb8911
Raymond Nugent <raymond.nugent@hotmail.com>
2022-01-30 19:26:08 +1000

    commit

--- utils/onerror.js
+++ utils/onerror.js
@@ -1,13 +1,8 @@
 import db from './db';
 
-const getError = (err) =>
-  err.response && err.response.data && err.response.data.message
-    ? err.response.data.message
-    : err.message;
-
 const onError = async (err, req, res) => {
   await db.disconnect();
   res.status(500).send({ message: err.toString() });
 };
 // eslint-disable-next-line react-hooks/exhaustive-deps
-export { getError, onError };
+export default onError;
