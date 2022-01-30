e5add31c11a7019ad7460df4e11108081038584a
Raymond Nugent <raymond.nugent@hotmail.com>
2022-01-30 18:51:26 +1000

    next.config fix

--- next.config.js
+++ next.config.js
@@ -1,2 +1,4 @@
-export const reactStrictMode = true;
-export const images = { domains: ['res.cloudinary.com'] };
+module.exports = {
+  reactStrictMode: true,
+  images: { domains: ['res.cloudinary.com'] },
+};
